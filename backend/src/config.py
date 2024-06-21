fb84ae18e9b6b3e663e2260ebc654abc62bb95ba
Shin Kranel <shinkranel@gmail.com>
2024-06-21 10:01:12 +0100

    Update config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,9 +1,12 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     DB_HOST: str
-    DB_PORT: int
+    DB_PORT: str
     DB_NAME: str
     DB_USER: str
     DB_PASS: str
@@ -18,7 +21,7 @@ class Settings(BaseSettings):
         # postgres+psycopg//user:pass@host:port/name
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file="../../.env")
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
