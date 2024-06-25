9af3603cb9f186d018d3ddbb0e095b692d974108
Shin Kranel <shinkranel@gmail.com>
2024-06-25 13:05:40 +0100

    Create func for get secret expire datetime (7 days by default)

--- backend/src/secrets/utils.py
+++ backend/src/secrets/utils.py
@@ -1,5 +1,13 @@
+from datetime import datetime, timedelta
+
 from backend.src.config import settings
 
 
 def get_secret_link(secret_id):
     return f"{settings.SECRET_LINK_DEV}{secret_id}"
+
+
+def get_expire_datetime():
+    create_datetime = datetime.now()
+    expire_datetime = create_datetime + timedelta(days=7)
+    return expire_datetime
